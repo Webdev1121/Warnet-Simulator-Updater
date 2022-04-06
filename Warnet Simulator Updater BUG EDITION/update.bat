377a797823ca4b6e7437fb073d57b945b75470f0
Dev1121 <95273407+Webdev1121@users.noreply.github.com>
2022-04-06 12:48:09 +0800

    Warnet Simulator Updater v1.5.1 BUG EDITION
    SEMOGA TIDAK ADA BUG **(╯°□°）╯︵ ┻━┻**

---
+++ Warnet Simulator Updater BUG EDITION/update.bat
@@ -0,0 +1,17 @@
+@echo off
+color 4
+echo WARNING! JANGAN HAPUS FOLDER "curl-7.82.0-win64-mingw"
+pause
+cls
+color b
+echo **********************************************************************************
+echo =========================Di bikin oleh 1121Developer#3978=========================
+echo ==================================================================================
+echo **********************************************************************************
+echo Jangan protes kalo jelec
+echo nanti ada file Warnet-new.rar di Desktop Anda.
+start C:\warnet\bin\curl.exe https://salamdaribinjaigames.000webhostapp.com/files/warnet/warnet-new.rar -o %USERPROFILE%\Desktop\warnet-new.rar
+
+pause
+echo Silahkan cek folder %USERPROFILE%\Desktop, ada file warnet-new.rar ,kalo ga ada file warnet-new.rar, coba pake update.bat
+pause
