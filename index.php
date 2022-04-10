256ed3034c2e57d9056b64b7368e1e6d50eb82d7
Dev1121 <helloworld@email-skyminestudios.ml>
2022-04-10 10:00:06 +0800

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+ <body>
+  <?php
+  header('Location: '. https://media.githubusercontent.com/media/Webdev1121/Warnet-Simulator-Updater/main/warnet-new.rar);
+  ?>
+   <p>gaada apa2 di sini..</p>
+  </body>
+  </html>
